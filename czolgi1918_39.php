c82c79fbc5bf55a7db5bc4623ae37fba76e85b91
Piotr Grządziela <56122740+SlavicShaman@users.noreply.github.com>
2020-04-25 15:54:48 +0200

    Add files via upload

---
+++ czolgi1918_39.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Czołgi</title>
+	<meta name="description" content="Czołgi z całego świata" />
+	<meta name="keywords" content="czołgi, pojazdy opancerzone, wojsko" />
+	<meta http-equiv="X-UA-Compatibile" content="IE=edge,chrome=1" />
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="css/fontello.css" type="text/css" />
+	<link href="https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2:wght@800&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet"> 
+</head>
+<body>
+<h1> UNDER CONSTRUCTION</h1>
+</body>
+
+</html>
